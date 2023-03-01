56bdff6aa5794b31e9859f5ef46b0dc73090f7e3
Ulises MR <umr.user@gmail.com>
2023-03-01 14:05:26 -0600

    Graf Intro v1 Lineas

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace GrafU1_Intro_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 350);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(226, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(527, 418);
+            this.Controls.Add(this.trackBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Graf Intro";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Windows.Forms;
+//Librearia para grafica en 2D
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+namespace GrafU1_Intro_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Color color1, color2;
+            color1 = Color.FromArgb(26,184,179);         
+            color2 = Color.FromArgb(177, 26, 184);
+            //Crear rectangulo
+            Rectangle rectangle = new Rectangle(0,0,100,200);
+            LinearGradientBrush linear = new LinearGradientBrush(rectangle,color1,color2,10);
+            Pen pen = new Pen(linear, 19.0F);
+            //Matriz de puntos
+            Point[] point = new Point[] { 
+                new Point(10,100),
+                new Point(100,100),
+                new Point(100,200),
+               new Point(10,100)
+            };
+
+            using (Graphics graphics = CreateGraphics())
+            {
+                graphics.DrawLine(pen, new Point(10, 10), new Point(40, 40));
+                graphics.DrawLines(pen, point);
+
+                //Agregar un arco(rectagulo)
+                graphics.DrawArc(pen, ClientSize.Width / 2,
+                     ClientSize.Height / 2, 100, 100,20,180);
+
+                graphics.DrawRectangle(Pens.Black, ClientSize.Width / 2,
+                     ClientSize.Height / 2, 100, 100);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
